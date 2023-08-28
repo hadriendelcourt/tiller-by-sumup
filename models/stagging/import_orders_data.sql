735b2e1cf9926c909599d2d128feb12fb58f38c1
hadriendelcourt@gmail.com <hadriendelcourt@gmail.com>
2023-08-28 13:36:24 +0000

    cleaned order data and store data : removed country and currency as always france and €; changed names for more clear view

--- models/stagging/import_orders_data.sql
+++ models/stagging/import_orders_data.sql
@@ -1,2 +1,15 @@
-SELECT *
+{{config (schema = 'tiller')}}
+SELECT id_order
+, id_store
+, id_table
+, id_waiter
+, id_device
+, date_opened
+, date_closed
+, dim_status
+, dim_source
+, m_nb_customer	AS nb_customer
+, m_cached_payed AS	payed
+, m_cached_price AS price
+, ROUND(SAFE_DIVIDE(m_cached_price,m_nb_customer),2) AS price_per_customer 
 FROM `tiller-by-sumup.tiller.order_data`
